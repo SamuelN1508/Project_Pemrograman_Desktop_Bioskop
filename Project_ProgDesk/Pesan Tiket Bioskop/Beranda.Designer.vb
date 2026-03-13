1b01fe78441c6031017340fec0d136a4a7a0dfbc
Yesi5 <143603528+Yesi5@users.noreply.github.com>
2026-03-13 15:09:08 +0700

    beranda

--- Project_ProgDesk/Pesan Tiket Bioskop/Beranda.Designer.vb
+++ Project_ProgDesk/Pesan Tiket Bioskop/Beranda.Designer.vb
@@ -1,9 +1,8 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Beranda
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -14,17 +13,215 @@ Partial Class Beranda
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Beranda"
+        LblTitle = New Label()
+        FlpMovies = New FlowLayoutPanel()
+        PnlTop = New Panel()
+        MenuStripUser = New MenuStrip()
+        MenuAkun = New ToolStripMenuItem()
+        MenuProfil = New ToolStripMenuItem()
+        MenuHistory = New ToolStripMenuItem()
+        MenuAbout = New ToolStripMenuItem()
+        SepLogout = New ToolStripSeparator()
+        MenuLogout = New ToolStripMenuItem()
+        PnlSearch = New Panel()
+        TxtCari = New TextBox()
+        BtnCari = New Button()
+        LblFilter = New Label()
+        CmbGenre = New ComboBox()
+        PnlTop.SuspendLayout()
+        MenuStripUser.SuspendLayout()
+        PnlSearch.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' LblTitle
+        ' 
+        LblTitle.AutoSize = True
+        LblTitle.Font = New Font("Segoe UI Semibold", 18.0F, FontStyle.Bold)
+        LblTitle.ForeColor = Color.FromArgb(45, 45, 45)
+        LblTitle.Location = New Point(25, 20)
+        LblTitle.Name = "LblTitle"
+        LblTitle.Size = New Size(224, 41)
+        LblTitle.TabIndex = 0
+        LblTitle.Text = "Sedang Tayang"
+        ' 
+        ' FlpMovies
+        ' 
+        FlpMovies.AutoScroll = True
+        FlpMovies.BackColor = Color.FromArgb(245, 247, 250)
+        FlpMovies.Dock = DockStyle.Fill
+        FlpMovies.Location = New Point(0, 90)
+        FlpMovies.Name = "FlpMovies"
+        FlpMovies.Padding = New Padding(30, 20, 30, 20)
+        FlpMovies.Size = New Size(1000, 610)
+        FlpMovies.TabIndex = 1
+        ' 
+        ' PnlTop
+        ' 
+        PnlTop.BackColor = Color.White
+        PnlTop.Controls.Add(MenuStripUser)
+        PnlTop.Controls.Add(PnlSearch)
+        PnlTop.Controls.Add(LblFilter)
+        PnlTop.Controls.Add(CmbGenre)
+        PnlTop.Controls.Add(LblTitle)
+        PnlTop.Dock = DockStyle.Top
+        PnlTop.Location = New Point(0, 0)
+        PnlTop.Name = "PnlTop"
+        PnlTop.Size = New Size(1000, 90)
+        PnlTop.TabIndex = 0
+        ' 
+        ' MenuStripUser
+        ' 
+        MenuStripUser.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        MenuStripUser.BackColor = Color.Transparent
+        MenuStripUser.Dock = DockStyle.None
+        MenuStripUser.ImageScalingSize = New Size(20, 20)
+        MenuStripUser.Items.AddRange(New ToolStripItem() {MenuAkun})
+        MenuStripUser.Location = New Point(860, 28)
+        MenuStripUser.Name = "MenuStripUser"
+        MenuStripUser.Size = New Size(116, 31)
+        MenuStripUser.TabIndex = 5
+        ' 
+        ' MenuAkun
+        ' 
+        MenuAkun.BackColor = Color.FromArgb(235, 235, 235)
+        MenuAkun.DropDownItems.AddRange(New ToolStripItem() {MenuProfil, MenuHistory, MenuAbout, SepLogout, MenuLogout})
+        MenuAkun.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        MenuAkun.ForeColor = Color.FromArgb(64, 64, 64)
+        MenuAkun.Margin = New Padding(0, 0, 10, 0)
+        MenuAkun.Name = "MenuAkun"
+        MenuAkun.Padding = New Padding(10, 4, 10, 4)
+        MenuAkun.Size = New Size(108, 31)
+        MenuAkun.Text = "Akun 👤"
+        ' 
+        ' MenuProfil
+        ' 
+        MenuProfil.Name = "MenuProfil"
+        MenuProfil.Size = New Size(224, 28)
+        MenuProfil.Text = "Profil Saya"
+        ' 
+        ' MenuHistory
+        ' 
+        MenuHistory.Name = "MenuHistory"
+        MenuHistory.Size = New Size(224, 28)
+        MenuHistory.Text = "Riwayat Tiket"
+        ' 
+        ' MenuAbout
+        ' 
+        MenuAbout.Name = "MenuAbout"
+        MenuAbout.Size = New Size(224, 28)
+        MenuAbout.Text = "Tentang Kami"
+        ' 
+        ' SepLogout
+        ' 
+        SepLogout.Name = "SepLogout"
+        SepLogout.Size = New Size(221, 6)
+        ' 
+        ' MenuLogout
+        ' 
+        MenuLogout.ForeColor = Color.Crimson
+        MenuLogout.Name = "MenuLogout"
+        MenuLogout.Size = New Size(224, 28)
+        MenuLogout.Text = "Logout"
+        ' 
+        ' PnlSearch
+        ' 
+        PnlSearch.BackColor = Color.FromArgb(240, 240, 240)
+        PnlSearch.Controls.Add(TxtCari)
+        PnlSearch.Controls.Add(BtnCari)
+        PnlSearch.Location = New Point(270, 25)
+        PnlSearch.Name = "PnlSearch"
+        PnlSearch.Size = New Size(300, 36)
+        PnlSearch.TabIndex = 1
+        ' 
+        ' TxtCari
+        ' 
+        TxtCari.BackColor = Color.FromArgb(240, 240, 240)
+        TxtCari.BorderStyle = BorderStyle.None
+        TxtCari.Font = New Font("Segoe UI", 10.0F)
+        TxtCari.Location = New Point(10, 7)
+        TxtCari.Name = "TxtCari"
+        TxtCari.PlaceholderText = "Cari film favorit..."
+        TxtCari.Size = New Size(215, 23)
+        TxtCari.TabIndex = 1
+        ' 
+        ' BtnCari
+        ' 
+        BtnCari.BackColor = Color.FromArgb(0, 150, 136)
+        BtnCari.Cursor = Cursors.Hand
+        BtnCari.Dock = DockStyle.Right
+        BtnCari.FlatAppearance.BorderSize = 0
+        BtnCari.FlatStyle = FlatStyle.Flat
+        BtnCari.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold)
+        BtnCari.ForeColor = Color.White
+        BtnCari.Location = New Point(230, 0)
+        BtnCari.Name = "BtnCari"
+        BtnCari.Size = New Size(70, 36)
+        BtnCari.TabIndex = 2
+        BtnCari.Text = "Cari"
+        BtnCari.UseVisualStyleBackColor = False
+        ' 
+        ' LblFilter
+        ' 
+        LblFilter.AutoSize = True
+        LblFilter.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular)
+        LblFilter.ForeColor = Color.Gray
+        LblFilter.Location = New Point(590, 33)
+        LblFilter.Name = "LblFilter"
+        LblFilter.Size = New Size(51, 20)
+        LblFilter.TabIndex = 3
+        LblFilter.Text = "Genre:"
+        ' 
+        ' CmbGenre
+        ' 
+        CmbGenre.BackColor = Color.White
+        CmbGenre.DropDownStyle = ComboBoxStyle.DropDownList
+        CmbGenre.FlatStyle = FlatStyle.Flat
+        CmbGenre.Font = New Font("Segoe UI", 10.0F)
+        CmbGenre.FormattingEnabled = True
+        CmbGenre.Items.AddRange(New Object() {"Semua", "Action", "Horror", "Drama", "Sci-Fi"})
+        CmbGenre.Location = New Point(647, 28)
+        CmbGenre.Name = "CmbGenre"
+        CmbGenre.Size = New Size(140, 31)
+        CmbGenre.TabIndex = 4
+        ' 
+        ' Beranda
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(1000, 700)
+        Controls.Add(FlpMovies)
+        Controls.Add(PnlTop)
+        MainMenuStrip = MenuStripUser
+        Name = "Beranda"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "CinemaHub - Dashboard"
+        PnlTop.ResumeLayout(False)
+        PnlTop.PerformLayout()
+        MenuStripUser.ResumeLayout(False)
+        MenuStripUser.PerformLayout()
+        PnlSearch.ResumeLayout(False)
+        PnlSearch.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents PnlTop As Panel
+    Friend WithEvents LblTitle As Label
+    Friend WithEvents PnlSearch As Panel
+    Friend WithEvents TxtCari As TextBox
+    Friend WithEvents BtnCari As Button
+    Friend WithEvents LblFilter As Label
+    Friend WithEvents CmbGenre As ComboBox
+    Friend WithEvents FlpMovies As FlowLayoutPanel
+    Friend WithEvents MenuStripUser As MenuStrip
+    Friend WithEvents MenuAkun As ToolStripMenuItem
+    Friend WithEvents MenuProfil As ToolStripMenuItem
+    Friend WithEvents MenuHistory As ToolStripMenuItem
+    Friend WithEvents MenuAbout As ToolStripMenuItem
+    Friend WithEvents SepLogout As ToolStripSeparator
+    Friend WithEvents MenuLogout As ToolStripMenuItem
 End Class
--- Project_ProgDesk/Pesan Tiket Bioskop/Beranda.vb
+++ Project_ProgDesk/Pesan Tiket Bioskop/Beranda.vb
@@ -1,3 +1,155 @@
-﻿Public Class Beranda
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Beranda
+
+    ' Sesuaikan database_name dengan nama database kamu di XAMPP
+    Dim connectionString As String = "server=localhost;user id=root;password=;database=bioskop_db"
+    Dim conn As MySqlConnection
+
+    Private Sub Beranda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Inisialisasi ComboBox
+        CmbGenre.SelectedIndex = 0
+        ' Load data pertama kali
+        MuatDaftarFilmDariDB()
+    End Sub
+
+    ''' <summary>
+    ''' Mengambil data film dari tabel sesuai screenshot (ID_Film, Judul_Film, Genre, Durasi_Menit)
+    ''' </summary>
+    Private Sub MuatDaftarFilmDariDB(Optional kataKunci As String = "", Optional filterGenre As String = "Semua")
+        FlpMovies.Controls.Clear()
+
+        Try
+            conn = New MySqlConnection(connectionString)
+            conn.Open()
+
+            ' Query SQL menyesuaikan nama kolom di gambar
+            Dim query As String = "SELECT * FROM movies WHERE Judul_Film LIKE @cari"
+
+            ' Logika filter genre
+            If filterGenre <> "Semua" Then
+                query &= " AND Genre = @genre"
+            End If
+
+            Dim cmd As New MySqlCommand(query, conn)
+            cmd.Parameters.AddWithValue("@cari", "%" & kataKunci & "%")
+
+            If filterGenre <> "Semua" Then
+                cmd.Parameters.AddWithValue("@genre", filterGenre)
+            End If
+
+            Dim adapter As New MySqlDataAdapter(cmd)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+
+            ' Render setiap baris database menjadi kartu film
+            For Each row As DataRow In table.Rows
+                BuatCardFilm(
+                    row("ID_Film").ToString(),
+                    row("Judul_Film").ToString(),
+                    row("Genre").ToString(),
+                    row("Durasi_Menit").ToString()
+                )
+            Next
+
+            ' Jika data kosong
+            If table.Rows.Count = 0 Then
+                Dim lblEmpty As New Label With {.Text = "Film tidak ditemukan.", .AutoSize = True, .ForeColor = Color.Gray}
+                FlpMovies.Controls.Add(lblEmpty)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Koneksi XAMPP Gagal: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Membuat kartu film secara dinamis
+    ''' </summary>
+    Private Sub BuatCardFilm(id As String, judul As String, genre As String, durasi As String)
+        ' Container utama kartu
+        Dim pnlCard As New Panel With {
+            .Size = New Size(200, 250),
+            .BackColor = Color.White,
+            .Margin = New Padding(15),
+            .Cursor = Cursors.Default
+        }
+
+        ' Judul Film
+        Dim lblJudul As New Label With {
+            .Text = judul,
+            .Font = New Font("Segoe UI", 11, FontStyle.Bold),
+            .ForeColor = Color.FromArgb(40, 40, 40),
+            .Dock = DockStyle.Top,
+            .Height = 60,
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Padding = New Padding(5)
+        }
+
+        ' Info Genre & Durasi
+        Dim lblDetail As New Label With {
+            .Text = genre & " • " & durasi & " Menit",
+            .Font = New Font("Segoe UI", 8, FontStyle.Regular),
+            .ForeColor = Color.DimGray,
+            .Dock = DockStyle.Top,
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Height = 30
+        }
+
+        ' Tombol Pesan
+        Dim btnPesan As New Button With {
+            .Text = "PESAN TIKET",
+            .Dock = DockStyle.Bottom,
+            .Height = 40,
+            .BackColor = Color.Teal,
+            .ForeColor = Color.White,
+            .FlatStyle = FlatStyle.Flat,
+            .Font = New Font("Segoe UI", 9, FontStyle.Bold),
+            .Cursor = Cursors.Hand
+        }
+        btnPesan.FlatAppearance.BorderSize = 0
+
+        ' Simpan ID Film di tag tombol agar bisa digunakan saat pindah form
+        btnPesan.Tag = id
+
+        ' Event klik tombol pesan
+        AddHandler btnPesan.Click, Sub(sender As Object, e As EventArgs)
+                                       Dim idFilm = DirectCast(sender, Button).Tag
+                                       MessageBox.Show("Membuka jadwal untuk film ID: " & idFilm.ToString(), "Info")
+                                       ' Contoh: FormPesan.ShowID(idFilm)
+                                   End Sub
+
+        ' Tambahkan ke panel kartu
+        pnlCard.Controls.Add(lblDetail)
+        pnlCard.Controls.Add(lblJudul)
+        pnlCard.Controls.Add(btnPesan)
+
+        ' Tambahkan ke FlowLayoutPanel
+        FlpMovies.Controls.Add(pnlCard)
+    End Sub
+
+    ' --- EVENT HANDLERS UI ---
+
+    Private Sub BtnCari_Click(sender As Object, e As EventArgs) Handles BtnCari.Click
+        MuatDaftarFilmDariDB(TxtCari.Text, CmbGenre.SelectedItem.ToString())
+    End Sub
+
+    Private Sub CmbGenre_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbGenre.SelectedIndexChanged
+        If CmbGenre.SelectedItem IsNot Nothing Then
+            MuatDaftarFilmDariDB(TxtCari.Text, CmbGenre.SelectedItem.ToString())
+        End If
+    End Sub
+
+    ' Logout & Menu Handlers
+    Private Sub MenuLogout_Click(sender As Object, e As EventArgs) Handles MenuLogout.Click
+        Me.Hide()
+        ' FormLogin.Show()
+    End Sub
+
+    Private Sub MenuProfil_Click(sender As Object, e As EventArgs) Handles MenuProfil.Click
+        MessageBox.Show("Halaman Profil Belum Tersedia", "Info")
+    End Sub
 
 End Class
