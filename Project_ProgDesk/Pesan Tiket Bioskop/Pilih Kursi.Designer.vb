4bfbd285f08f84a0c80d047e6f68dc3b9f39d7d7
Yesi5 <143603528+Yesi5@users.noreply.github.com>
2026-03-13 17:43:59 +0700

    pilihKursi

--- Project_ProgDesk/Pesan Tiket Bioskop/Pilih Kursi.Designer.vb
+++ Project_ProgDesk/Pesan Tiket Bioskop/Pilih Kursi.Designer.vb
@@ -1,9 +1,8 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Pilih_Kursi
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class PilihKursi
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -14,17 +13,265 @@ Partial Class Pilih_Kursi
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Pilih_Kursi"
+        LblTitle = New Label()
+        PnlLayar = New Panel()
+        LblLayar = New Label()
+        PnlLegend = New Panel()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        BoxTerblokir = New PictureBox()
+        BoxDipilih = New PictureBox()
+        BoxDipesan = New PictureBox()
+        BoxTersedia = New PictureBox()
+        PnlKursi = New Panel()
+        LblKursiDipilih = New Label()
+        PnlDetail = New Panel()
+        LblDetailInfo = New Label()
+        LblDetailJudul = New Label()
+        BtnLanjut = New Button()
+        PnlLayar.SuspendLayout()
+        PnlLegend.SuspendLayout()
+        CType(BoxTerblokir, ComponentModel.ISupportInitialize).BeginInit()
+        CType(BoxDipilih, ComponentModel.ISupportInitialize).BeginInit()
+        CType(BoxDipesan, ComponentModel.ISupportInitialize).BeginInit()
+        CType(BoxTersedia, ComponentModel.ISupportInitialize).BeginInit()
+        PnlDetail.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' LblTitle
+        ' 
+        LblTitle.AutoSize = True
+        LblTitle.Font = New Font("Segoe UI", 16.0F, FontStyle.Bold)
+        LblTitle.Location = New Point(20, 15)
+        LblTitle.Name = "LblTitle"
+        LblTitle.Size = New Size(280, 37)
+        LblTitle.TabIndex = 0
+        LblTitle.Text = "Halaman Pilih Kursi"
+        ' 
+        ' PnlLayar
+        ' 
+        PnlLayar.Controls.Add(LblLayar)
+        PnlLayar.Location = New Point(30, 70)
+        PnlLayar.Name = "PnlLayar"
+        PnlLayar.Size = New Size(540, 30)
+        PnlLayar.TabIndex = 1
+        ' 
+        ' LblLayar
+        ' 
+        LblLayar.AutoSize = True
+        LblLayar.BackColor = Color.FromArgb(240, 240, 240)
+        LblLayar.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold)
+        LblLayar.Location = New Point(245, 5)
+        LblLayar.Name = "LblLayar"
+        LblLayar.Size = New Size(47, 20)
+        LblLayar.TabIndex = 0
+        LblLayar.Text = "Layar"
+        ' 
+        ' PnlLegend
+        ' 
+        PnlLegend.Controls.Add(Label5)
+        PnlLegend.Controls.Add(Label6)
+        PnlLegend.Controls.Add(Label7)
+        PnlLegend.Controls.Add(Label8)
+        PnlLegend.Controls.Add(BoxTerblokir)
+        PnlLegend.Controls.Add(BoxDipilih)
+        PnlLegend.Controls.Add(BoxDipesan)
+        PnlLegend.Controls.Add(BoxTersedia)
+        PnlLegend.Location = New Point(680, 110)
+        PnlLegend.Name = "PnlLegend"
+        PnlLegend.Size = New Size(120, 140)
+        PnlLegend.TabIndex = 2
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(35, 105)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(69, 20)
+        Label5.TabIndex = 7
+        Label5.Text = "Terblokir"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(35, 75)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(53, 20)
+        Label6.TabIndex = 6
+        Label6.Text = "Dipilih"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(35, 45)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(66, 20)
+        Label7.TabIndex = 5
+        Label7.Text = "Dipesan"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(35, 15)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(64, 20)
+        Label8.TabIndex = 4
+        Label8.Text = "Tersedia"
+        ' 
+        ' BoxTerblokir
+        ' 
+        BoxTerblokir.BackColor = Color.Gray
+        BoxTerblokir.BorderStyle = BorderStyle.FixedSingle
+        BoxTerblokir.Location = New Point(10, 105)
+        BoxTerblokir.Name = "BoxTerblokir"
+        BoxTerblokir.Size = New Size(20, 20)
+        BoxTerblokir.TabIndex = 3
+        BoxTerblokir.TabStop = False
+        ' 
+        ' BoxDipilih
+        ' 
+        BoxDipilih.BackColor = Color.FromArgb(46, 139, 135)
+        BoxDipilih.BorderStyle = BorderStyle.FixedSingle
+        BoxDipilih.Location = New Point(10, 75)
+        BoxDipilih.Name = "BoxDipilih"
+        BoxDipilih.Size = New Size(20, 20)
+        BoxDipilih.TabIndex = 2
+        BoxDipilih.TabStop = False
+        ' 
+        ' BoxDipesan
+        ' 
+        BoxDipesan.BackColor = Color.IndianRed
+        BoxDipesan.BorderStyle = BorderStyle.FixedSingle
+        BoxDipesan.Location = New Point(10, 45)
+        BoxDipesan.Name = "BoxDipesan"
+        BoxDipesan.Size = New Size(20, 20)
+        BoxDipesan.TabIndex = 1
+        BoxDipesan.TabStop = False
+        ' 
+        ' BoxTersedia
+        ' 
+        BoxTersedia.BackColor = Color.White
+        BoxTersedia.BorderStyle = BorderStyle.FixedSingle
+        BoxTersedia.Location = New Point(10, 15)
+        BoxTersedia.Name = "BoxTersedia"
+        BoxTersedia.Size = New Size(20, 20)
+        BoxTersedia.TabIndex = 0
+        BoxTersedia.TabStop = False
+        ' 
+        ' PnlKursi
+        ' 
+        PnlKursi.Location = New Point(25, 110)
+        PnlKursi.Name = "PnlKursi"
+        PnlKursi.Size = New Size(650, 320)
+        PnlKursi.TabIndex = 3
+        ' 
+        ' LblKursiDipilih
+        ' 
+        LblKursiDipilih.AutoSize = True
+        LblKursiDipilih.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        LblKursiDipilih.Location = New Point(25, 460)
+        LblKursiDipilih.Name = "LblKursiDipilih"
+        LblKursiDipilih.Size = New Size(171, 23)
+        LblKursiDipilih.TabIndex = 4
+        LblKursiDipilih.Text = "Kursi yang dipilih: -"
+        ' 
+        ' PnlDetail
+        ' 
+        PnlDetail.BackColor = Color.White
+        PnlDetail.BorderStyle = BorderStyle.FixedSingle
+        PnlDetail.Controls.Add(LblDetailInfo)
+        PnlDetail.Controls.Add(LblDetailJudul)
+        PnlDetail.Location = New Point(525, 440)
+        PnlDetail.Name = "PnlDetail"
+        PnlDetail.Size = New Size(300, 60)
+        PnlDetail.TabIndex = 5
+        ' 
+        ' LblDetailInfo
+        ' 
+        LblDetailInfo.AutoSize = True
+        LblDetailInfo.Font = New Font("Segoe UI", 9.0F)
+        LblDetailInfo.Location = New Point(5, 30)
+        LblDetailInfo.Name = "LblDetailInfo"
+        LblDetailInfo.Size = New Size(195, 20)
+        LblDetailInfo.TabIndex = 1
+        LblDetailInfo.Text = "Film Title | Studio 1 | 14:00"
+        ' 
+        ' LblDetailJudul
+        ' 
+        LblDetailJudul.AutoSize = True
+        LblDetailJudul.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold)
+        LblDetailJudul.Location = New Point(5, 5)
+        LblDetailJudul.Name = "LblDetailJudul"
+        LblDetailJudul.Size = New Size(140, 20)
+        LblDetailJudul.TabIndex = 0
+        LblDetailJudul.Text = "Detail Pembayaran"
+        ' 
+        ' BtnLanjut
+        ' 
+        BtnLanjut.BackColor = Color.FromArgb(46, 139, 135)
+        BtnLanjut.FlatAppearance.BorderSize = 0
+        BtnLanjut.FlatStyle = FlatStyle.Flat
+        BtnLanjut.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
+        BtnLanjut.ForeColor = Color.White
+        BtnLanjut.Location = New Point(25, 520)
+        BtnLanjut.Name = "BtnLanjut"
+        BtnLanjut.Size = New Size(800, 45)
+        BtnLanjut.TabIndex = 6
+        BtnLanjut.Text = "Lanjut ke Pembayaran"
+        BtnLanjut.UseVisualStyleBackColor = False
+        ' 
+        ' PilihKursi
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(240, 240, 240)
+        ClientSize = New Size(850, 600)
+        Controls.Add(BtnLanjut)
+        Controls.Add(PnlDetail)
+        Controls.Add(LblKursiDipilih)
+        Controls.Add(PnlKursi)
+        Controls.Add(PnlLegend)
+        Controls.Add(PnlLayar)
+        Controls.Add(LblTitle)
+        Name = "PilihKursi"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Pilih Kursi"
+        PnlLayar.ResumeLayout(False)
+        PnlLayar.PerformLayout()
+        PnlLegend.ResumeLayout(False)
+        PnlLegend.PerformLayout()
+        CType(BoxTerblokir, ComponentModel.ISupportInitialize).EndInit()
+        CType(BoxDipilih, ComponentModel.ISupportInitialize).EndInit()
+        CType(BoxDipesan, ComponentModel.ISupportInitialize).EndInit()
+        CType(BoxTersedia, ComponentModel.ISupportInitialize).EndInit()
+        PnlDetail.ResumeLayout(False)
+        PnlDetail.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents LblTitle As Label
+    Friend WithEvents PnlLayar As Panel
+    Friend WithEvents LblLayar As Label
+    Friend WithEvents PnlLegend As Panel
+    Friend WithEvents BoxTerblokir As PictureBox
+    Friend WithEvents BoxDipilih As PictureBox
+    Friend WithEvents BoxDipesan As PictureBox
+    Friend WithEvents BoxTersedia As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents PnlKursi As Panel
+    Friend WithEvents LblKursiDipilih As Label
+    Friend WithEvents PnlDetail As Panel
+    Friend WithEvents LblDetailInfo As Label
+    Friend WithEvents LblDetailJudul As Label
+    Friend WithEvents BtnLanjut As Button
 End Class
--- Project_ProgDesk/Pesan Tiket Bioskop/Pilih Kursi.vb
+++ Project_ProgDesk/Pesan Tiket Bioskop/Pilih Kursi.vb
@@ -1,3 +1,165 @@
-﻿Public Class Pilih_Kursi
+﻿Imports MySql.Data.MySqlClient
+
+Public Class PilihKursi
+
+    ' --- DATA DARI FORM SEBELUMNYA ---
+    Public JudulFilmTerpilih As String = "Tim Tim"
+    Public InfoJadwal As String = "Sindia, 11 12 Ster | 14:00"
+    Public IdStudioTerpilih As String = "1"
+
+    ' --- PENYIMPANAN SEMENTARA ---
+    Private ListKursiDipilih As New List(Of String)
+
+    ' --- PENGATURAN WARNA KURSI ---
+    Private WarnaTersedia As Color = Color.White
+    Private WarnaDipesan As Color = Color.IndianRed
+    Private WarnaDipilih As Color = Color.FromArgb(46, 139, 135) ' Tosca Gelap
+    Private WarnaTerblokir As Color = Color.Gray
+
+    ' --- KONFIGURASI DATABASE ---
+    ' Sesuaikan 'database=db_bioskop' dengan nama database Anda di phpMyAdmin
+    Dim connectionString As String = "server=localhost;user id=root;password=;database=bioskop_db"
+    Dim conn As MySqlConnection
+
+    Private Sub PilihKursi_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set label detail film
+        LblDetailInfo.Text = JudulFilmTerpilih & " | " & InfoJadwal
+
+        ' Buat Garis Layar
+        Dim line As New Label With {.BorderStyle = BorderStyle.Fixed3D, .Height = 2, .Width = 540, .Top = 15, .Left = 0}
+        PnlLayar.Controls.Add(line)
+        LblLayar.BringToFront()
+
+        ' 1. Bikin semua tombol kursi (A1 sampai H14)
+        GenerateKursi()
+
+        ' 2. Cek database untuk melihat kursi mana yang Aktif (Tersedia) atau Non-Aktif (Terblokir)
+        MuatStatusKursiDariDB()
+    End Sub
+
+    Private Sub GenerateKursi()
+        Dim baris As String() = {"A", "B", "C", "D", "E", "F", "G", "H"}
+        Dim jumlahKolom As Integer = 14
+
+        Dim ukuranKursi As Integer = 30
+        Dim jarak As Integer = 6
+
+        For i As Integer = 0 To baris.Length - 1
+            Dim posisiX As Integer = 0
+
+            For j As Integer = 1 To jumlahKolom
+                ' Lorong (Jeda Kosong)
+                If j = 3 Then posisiX += 20
+                If j = 13 Then posisiX += 20
+
+                Dim btnKursi As New Button() With {
+                    .Name = "Btn_" & baris(i) & j.ToString(),
+                    .Text = "",
+                    .Tag = baris(i) & j.ToString(), ' Tag menyimpan Kode_Kursi (contoh: "A1")
+                    .Size = New Size(ukuranKursi, ukuranKursi),
+                    .Location = New Point(posisiX, i * (ukuranKursi + jarak)),
+                    .BackColor = WarnaTersedia,
+                    .FlatStyle = FlatStyle.Flat,
+                    .Cursor = Cursors.Hand
+                }
+                btnKursi.FlatAppearance.BorderColor = Color.DarkGray
+
+                ' Event Klik
+                AddHandler btnKursi.Click, AddressOf Kursi_Click
+
+                PnlKursi.Controls.Add(btnKursi)
+
+                posisiX += ukuranKursi + jarak
+            Next
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Mengambil status kursi dari tabel 'seats' berdasarkan ID_Studio
+    ''' </summary>
+    Private Sub MuatStatusKursiDariDB()
+        Try
+            conn = New MySqlConnection(connectionString)
+            conn.Open()
+
+            ' Ambil data kursi untuk studio yang dipilih
+            Dim query As String = "SELECT Kode_Kursi, Status_Aktif FROM seats WHERE ID_Studio = @idStudio"
+            Dim cmd As New MySqlCommand(query, conn)
+            cmd.Parameters.AddWithValue("@idStudio", IdStudioTerpilih)
+
+            Dim reader As MySqlDataReader = cmd.ExecuteReader()
+
+            ' Looping data dari database
+            While reader.Read()
+                Dim kodeKursi As String = reader("Kode_Kursi").ToString()
+                Dim statusAktif As Integer = Convert.ToInt32(reader("Status_Aktif"))
+
+                ' Cari tombol di panel yang Tag-nya sama dengan Kode_Kursi di DB
+                For Each ctrl As Control In PnlKursi.Controls
+                    If TypeOf ctrl Is Button Then
+                        Dim btn As Button = DirectCast(ctrl, Button)
+
+                        If btn.Tag IsNot Nothing AndAlso btn.Tag.ToString() = kodeKursi Then
+                            ' Update warna berdasarkan Status_Aktif
+                            If statusAktif = 1 Then
+                                btn.BackColor = WarnaTersedia
+                            Else
+                                btn.BackColor = WarnaTerblokir
+                                btn.Cursor = Cursors.No ' Ubah kursor jadi tanda dilarang
+                            End If
+                            Exit For ' Lanjut ke data dari database berikutnya
+                        End If
+                    End If
+                Next
+            End While
+
+            reader.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Gagal terhubung ke Database: " & ex.Message, "Error XAMPP", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If conn.State = ConnectionState.Open Then conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Kursi_Click(sender As Object, e As EventArgs)
+        Dim btn As Button = DirectCast(sender, Button)
+        Dim nomorKursi As String = btn.Tag.ToString()
+
+        ' Tolak klik jika kursi dipesan (merah) atau terblokir (abu-abu)
+        If btn.BackColor = WarnaDipesan OrElse btn.BackColor = WarnaTerblokir Then
+            Return
+        End If
+
+        ' Logika Pilih / Batal (Putih <-> Tosca)
+        If btn.BackColor = WarnaTersedia Then
+            btn.BackColor = WarnaDipilih
+            ListKursiDipilih.Add(nomorKursi)
+        Else
+            btn.BackColor = WarnaTersedia
+            ListKursiDipilih.Remove(nomorKursi)
+        End If
+
+        UpdateTeksBawah()
+    End Sub
+
+    Private Sub UpdateTeksBawah()
+        If ListKursiDipilih.Count = 0 Then
+            LblKursiDipilih.Text = "Kursi yang dipilih: -"
+        Else
+            ListKursiDipilih.Sort()
+            LblKursiDipilih.Text = "Kursi yang dipilih: " & String.Join(", ", ListKursiDipilih)
+        End If
+    End Sub
+
+    Private Sub BtnLanjut_Click(sender As Object, e As EventArgs) Handles BtnLanjut.Click
+        If ListKursiDipilih.Count = 0 Then
+            MessageBox.Show("Pilih minimal 1 kursi terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        MessageBox.Show("Lanjut ke pembayaran. Kursi: " & String.Join(", ", ListKursiDipilih), "Sukses")
+        ' FormPembayaran.Show()
+    End Sub
 
 End Class
